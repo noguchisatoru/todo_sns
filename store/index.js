01ac5423b7c47053b7c04fd29caeca1e2aaf03af
野口慧 <satoru@noguchitoshinoMacBook-Pro.local>
2019-09-21 10:51:15 +0900

    storeのいらない部分を削除

--- store/index.js
+++ store/index.js
@@ -31,20 +31,11 @@ export default () =>
       [INIT_USER]: firestoreAction((context) => {
         return context.bindFirestoreRef('users', usersRef)
       }),
-      //   [INIT_BALANCE]: firestoreAction((context) => {
-      //     return context.bindFirestoreRef('balances', balanceRef)
-      //   }),
       [ADD_USER]: firestoreAction(async (context, userdata) => {
         try {
           await usersRef.doc(userdata.uId).set({
 
             userName: userdata.userName
-
-          })
-
-          await balanceRef.doc(userdata.uId).set({
-
-            // balance: DEFAULT_WALLET_AMOUNT
 
           })
         } catch (e) {
@@ -67,56 +58,5 @@ export default () =>
           console.log(e)
         }
       }
-
-      //   [SELECT_USERDATA]: async ({ commit }, uid) => {
-      //     try {
-      //       const selectdata = await Promise.all([
-      //         usersRef.doc(uid).get(),
-      //         balanceRef.doc(uid).get()
-      //       ])
-
-      //       const selectUserdata = {
-      //         uId: uid,
-      //         userName: selectdata[0].data().userName,
-      //         balance: selectdata[1].data().balance
-      //       }
-
-      //       return selectUserdata
-      //     } catch (e) {
-      //       console.log(e)
-      //     }
-      //   },
-      //   [SEND_WALLET]:firestoreAction(async (context, {wallet, uid}) => {
-      //     try{
-      //       const recieverId = context.state.user.uId;
-      //       const senderId = uid;
-
-      //       if(recieverId !== senderId){
-      //         const getIdData = await Promise.all([
-      //           balanceRef.doc(recieverId).get(),
-      //           balanceRef.doc(senderId).get()
-      //         ]);
-
-      //         const recieverBalance = getIdData[0].data().balance;
-      //         const senderBalance = getIdData[1].data().balance;
-      //         await Promise.all([
-      //           balanceRef.doc(recieverId).set({
-      //             balance: recieverBalance - Number(wallet)
-      //           }),
-      //           balanceRef.doc(senderId).set({
-      //             balance: senderBalance + Number(wallet)
-      //           })
-      //         ]);
-      //       }else{
-      //         alert("自分自身に送ることはできません！");
-      //       }
-
-      //     }catch(e){
-      //       console.log(e);
-      //     }
-      //   }),
-    //   [LOGOUT_USER]: ({commit}) => {
-    //     commit("logoutUser");
-    //   }
     }
   })
