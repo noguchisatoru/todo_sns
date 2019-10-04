3d05a3a359ac8709a002cac495749428621c8045
野口慧 <satoru@noguchitoshinoMacBook-Pro.local>
2019-10-04 23:28:19 +0900

    Modal追加

--- components/Mytodo.vue
+++ components/Mytodo.vue
@@ -1,38 +1,38 @@
 <template>
-    <section>
-  <article class="media" @click="isImageModalActive = true">
-    <div class="media-content">
-      <div class="content">
-        <p>
-          Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.
-        </p>
-      </div>
-      <nav class="level is-mobile">
-        <div class="level-left">
-          <p>tag</p>
+  <section>
+    <article class="media" @click="isImageModalActive = true">
+      <div class="media-content">
+        <div class="content">
+          <p>
+            Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.
+          </p>
         </div>
-        <div class="level-right">
-          <b-dropdown aria-role="list">
-            <button slot="trigger" class="button is-small">
-              <span>Click me!</span>
-              <b-icon icon="menu-down" />
-            </button>
+        <nav class="level is-mobile">
+          <div class="level-left">
+            <p>tag</p>
+          </div>
+          <div class="level-right">
+            <b-dropdown aria-role="list">
+              <button slot="trigger" class="button is-small">
+                <span>Click me!</span>
+                <b-icon icon="menu-down" />
+              </button>
 
-            <b-dropdown-item aria-role="listitem">
-              Action
-            </b-dropdown-item>
-            <b-dropdown-item aria-role="listitem">
-              Another action
-            </b-dropdown-item>
-            <b-dropdown-item aria-role="listitem">
-              Something else
-            </b-dropdown-item>
-          </b-dropdown>
-        </div>
-      </nav>
-    </div>
-  </article>
-   <b-modal :active.sync="isImageModalActive">
+              <b-dropdown-item aria-role="listitem">
+                Action
+              </b-dropdown-item>
+              <b-dropdown-item aria-role="listitem">
+                Another action
+              </b-dropdown-item>
+              <b-dropdown-item aria-role="listitem">
+                Something else
+              </b-dropdown-item>
+            </b-dropdown>
+          </div>
+        </nav>
+      </div>
+    </article>
+    <b-modal :active.sync="isImageModalActive">
       <p class="is-4by3">
         mytodo
       </p>
--- components/Todoarticle.vue
+++ components/Todoarticle.vue
@@ -1,28 +1,45 @@
 <template>
-  <article class="media">
-    <figure class="media-left">
-      <p class="image is-64x64">
-        <img src="https://bulma.io/images/placeholders/128x128.png">
-      </p>
-    </figure>
-    <div class="media-content">
-      <div class="content">
-        <p>
-          <strong>John Smith</strong> <small>@johnsmith</small> <small>31m</small> <small>tag</small>
-          <br>
-          Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.
+  <section>
+    <article class="media" @click="isImageModalActive = true">
+      <figure class="media-left">
+        <p class="image is-64x64">
+          <img src="https://bulma.io/images/placeholders/128x128.png">
         </p>
-      </div>
-      <nav class="level is-mobile">
-        <div class="level-left">
-          <a class="level-item">
-            <span class="icon is-small"><i class="fas fa-reply" /></span>
-          </a>
-          <a class="level-item">
-            <span class="icon is-small"><i class="fas fa-heart" /></span>
-          </a>
+      </figure>
+      <div class="media-content">
+        <div class="content">
+          <p>
+            <strong>John Smith</strong> <small>@johnsmith</small> <small>31m</small> <small>tag</small>
+            <br>
+            Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.
+          </p>
         </div>
-      </nav>
-    </div>
-  </article>
+        <nav class="level is-mobile">
+          <div class="level-left">
+            <a class="level-item">
+              <span class="icon is-small"><i class="fas fa-reply" /></span>
+            </a>
+            <a class="level-item">
+              <span class="icon is-small"><i class="fas fa-heart" /></span>
+            </a>
+          </div>
+        </nav>
+      </div>
+    </article>
+    <b-modal :active.sync="isImageModalActive">
+      <p class="image is-4by3">
+        <img src="/static/img/placeholder-1280x960.png">
+      </p>
+    </b-modal>
+  </section>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      isImageModalActive: false
+    }
+  }
+}
+</script>
