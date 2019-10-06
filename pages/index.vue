4e0fd3303d23fc28d3dcf7361e60bbf1fa1dbb1b
野口慧 <satoru@noguchitoshinoMacBook-Pro.local>
2019-10-07 03:16:50 +0900

    index.vueのいらない部分削除

--- pages/index.vue
+++ pages/index.vue
@@ -8,7 +8,6 @@
 </template>
 
 <script>
-import { auth } from '~/plugins/firebase'
 import Logo from '~/components/Logo.vue'
 import Login from '~/components/Login.vue'
 
@@ -17,66 +16,7 @@ export default {
   components: {
     Logo,
     Login
-  },
-  data () {
-    return {
-      username: '',
-      email: '',
-      password: '',
-
-      isActive: true
-    }
-  },
-
-  created () {
-    this.$store.dispatch('user/initUser')
-  },
-
-  methods: {
-    async doLogin () {
-      try {
-        const user = await auth().signInWithEmailAndPassword(this.email, this.password)
-        if (user) {
-          await this.$store.dispatch('user/setUserdata', auth().currentUser.uid)
-          this.$router.push('/home')
-        }
-      } catch (e) {
-        alert(e)
-      }
-    },
-
-    async addUser () {
-      try {
-        const user = await auth().createUserWithEmailAndPassword(this.email, this.password)
-        if (user) {
-          const userdata = auth().currentUser
-          this.$store.dispatch('user/addUser', { userName: this.username, uId: userdata.uid })
-          await this.$store.dispatch('user/setUserdata', userdata.uid)
-          alert('登録完了' + userdata.email)
-          this.$router.push('/home')
-        }
-      } catch (e) {
-        alert(e)
-      }
-    },
-
-    async googleLogin () {
-      try {
-        const googleAuth = new auth.GoogleAuthProvider()
-        const googleUser = await auth().signInWithPopup(googleAuth)
-        if (googleUser) {
-          const googleData = auth().currentUser
-          await this.$store.dispatch('user/addUser', { userName: googleData.displayName, uId: googleData.uid })
-          this.$router.push('/home')
-        }
-      } catch (e) {
-        alert(e)
-      }
-    },
-
-    changeDisplay () {
-      this.isActive = !this.isActive
-    }
   }
+
 }
 </script>
